cd6347d5fb5c9ad352efe62bc7818464b4d1d3c8
Sofian Hadianto <sofianhadianto@Admins-MacBook-Pro.local>
2022-08-25 13:31:55 +0700

    Project init

---
+++ src/main/java/com/alami/sofianhcodingtest/SofianhCodingTestApplication.java
@@ -0,0 +1,13 @@
+package com.alami.sofianhcodingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofianhCodingTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofianhCodingTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alami/sofianhcodingtest/SofianhCodingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alami.sofianhcodingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofianhCodingTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
