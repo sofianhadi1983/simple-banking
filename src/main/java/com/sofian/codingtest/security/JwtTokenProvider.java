c344296958ada75ae3be204bf55d131725ffc765
sofianhadi1983 <sofian.hadi1983@gmail.com>
2022-08-28 13:48:54 +0700

    Delete unused code

--- src/main/java/com/sofian/codingtest/security/JwtTokenProvider.java
+++
@@ -1,8 +0,0 @@
-package com.sofian.codingtest.security;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class JwtTokenProvider {
-
-}
--- src/main/java/com/sofian/codingtest/security/entities/RoleName.java
+++
@@ -1,6 +0,0 @@
-package com.sofian.codingtest.security.entities;
-
-public enum  RoleName {
-    ROLE_USER,
-    ROLE_ADMIN
-}
